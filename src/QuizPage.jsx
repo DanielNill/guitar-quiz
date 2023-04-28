ee14ff21b009053ee2066ef7db9fc6efc710cc2d
DanielN <daniellnill@gmail.com>
2023-04-28 13:07:29 -0700

    improve experience

--- src/QuizPage.jsx
+++ src/QuizPage.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import { scaleNumbers, scaleQualities, specificScaleQualities, strings } from './questions'
 
+let questions = []
+let answeredQuestions = []
 export default function QuizPage({ timeLimit, questionCategories, streakChallenge}) {
   const [question, setQuestion] = useState(null)
   const [options, setOptions] = useState([])
@@ -9,21 +11,24 @@ export default function QuizPage({ timeLimit, questionCategories, streakChalleng
   const [isAnswered, setIsAnswered] = useState(false)
   const [counter, setCounter] = useState(timeLimit/1000)
   const [answersDisabled, setAnswersDisabled] = useState(false)
-
+  const [correctCount, setCorrectCount] = useState(0)
+  const [totalCount, setTotalCount] = useState(0)
+  const [questionsRemaining, setQuestionsRemaining] = useState(0)
   let streak = 0
 
-  let questions = []
+  let qs = []
   questionCategories.forEach((cat) => {
     if (cat === 'scaleNumbers') {
-      questions = questions.concat(scaleNumbers)
+      qs = qs.concat(scaleNumbers)
     } else if (cat === 'scaleQualities') {
-      questions = questions.concat(scaleQualities)
+      qs = qs.concat(scaleQualities)
     } else if (cat === 'specificScaleQualities') {
-      questions = questions.concat(specificScaleQualities)
+      qs = qs.concat(specificScaleQualities)
     } else if (cat === 'strings') {
-      questions = questions.concat(strings)
+      qs = qs.concat(strings)
     }
   })
+  questions = qs.filter((q) => !answeredQuestions.includes(q.question))
 
   const getRandomQuestion = () => {
     const question = questions[Math.floor(Math.random() * questions.length)]
@@ -40,11 +45,15 @@ export default function QuizPage({ timeLimit, questionCategories, streakChalleng
     e.preventDefault()
     setIsAnswered(true)
     let answer = e.target.value
-    console.log(`${question} answer: ${answer} correctAnswer: ${correctAnswer}`)
+    setTotalCount(totalCount + 1)
     if (answer === correctAnswer) {
       localStorage.setItem('correct', Number(localStorage.getItem('correct')) + 1)
       setIsCorrect(true)
       setAnswersDisabled(true)
+      setCorrectCount(correctCount + 1)
+      answeredQuestions.push(question)
+      questions = questions.filter((q) => !answeredQuestions.includes(q.question))
+      setQuestionsRemaining(questions.length)
       streak++
       if (streak >= streakChallenge && streakChallenge > 0) {
         alert(`You've answered ${streak} questions in a row correctly!`)
@@ -69,6 +78,7 @@ export default function QuizPage({ timeLimit, questionCategories, streakChalleng
   }
 
   useEffect(() => {
+    setQuestionsRemaining(questions.length)
     getRandomQuestion()
   }, [])
 
@@ -76,6 +86,7 @@ export default function QuizPage({ timeLimit, questionCategories, streakChalleng
     if (counter > 0 && !isAnswered) {
       setTimeout(() => { setCounter(counter - 1) }, 1000)
     } else if (counter === 0 && !isAnswered) {
+      setTotalCount(totalCount + 1)
       handleWrongAnswer()
     }
   }, [counter])
@@ -85,7 +96,8 @@ export default function QuizPage({ timeLimit, questionCategories, streakChalleng
       {!isAnswered && ( <div>Time Remaining: {counter}</div> )}
       {isAnswered && isCorrect && ( <div className="green">Correct!</div> )}
       {isAnswered && !isCorrect && ( <div className="red">Incorrect! The correct answer was {correctAnswer}</div> )}
-
+      <div>Correct: {correctCount}/{totalCount}</div>
+      <div>{questionsRemaining} questions remaining</div>
       <div><h2>{question}</h2></div>
       <span className="buttons-container">
         {options.map((ans) => (
